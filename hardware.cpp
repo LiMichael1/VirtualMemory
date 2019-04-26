178d13786457197f70715b8939b9e9a9507007bf
dgwh W.H <dgwh1995@hotmail.com>
2019-04-26 01:16:34 -0700

    update 1

---
+++ hardware.cpp
@@ -0,0 +1,96 @@
+#include <iostream>
+
+
+class BackingStore
+{
+	private:
+		//
+
+	public:
+		BackingStore();
+		~BackingStore();
+		//instance();
+		//BackingStore operator=
+
+
+};
+
+
+
+class Ram
+{
+	private:
+		Struct Status
+		{
+			bool accessed;
+			bool dirty;
+		};
+		Status status;
+		
+	public:	
+
+};
+
+struct Word
+{
+	public:
+		Word(int x) : _value(x) {};
+		int uint32();
+		uint32_t _value;
+
+};
+
+int Word::uint32()
+{
+	return this->_value;
+}
+
+
+struct Address : public Word
+{
+	Word address();
+	Word displacement();
+	Word frame();
+	Word page();
+
+};
+
+Word Address::address()
+{
+	return this->_value;
+}
+
+/****************************************************
+bit mask 0000 0000 1111 1111  = 255
+
+return page index
+
+****************************************************/
+
+
+
+Word Address::frame() {
+	return this->_value & 255;
+}
+
+/****************************************************
+bit mask 1111 1111 0000 0000 = 65280
+
+shift right 8 times
+
+return page number
+
+****************************************************/
+Word Address::page() {
+	return (this->_value & 65280) >> 8;
+}
+
+class MemoryManagementUnit
+{
+	private:
+	public:
+		void clearTLB();
+
+};
+
+
