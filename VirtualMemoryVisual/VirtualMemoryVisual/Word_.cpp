57570d238e4b1aeb6868e88e57f4c9230ab6dc8e
Michael Li <limichael1099419@gmail.com>
2019-05-01 16:55:44 -0700

    unneccessary

--- VirtualMemoryVisual/VirtualMemoryVisual/Word_.cpp
+++
@@ -1,8 +0,0 @@
-#include "Word_.h"
-
-int Word_::uint32()
-{
-	return _value;
-}
-
-
--- VirtualMemoryVisual/VirtualMemoryVisual/Word_.h
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include <vector>
-
-struct Word_ {
-public:
-	Word_(int x) : _value(x) {};
-	int uint32();
-	uint32_t _value;
-};
