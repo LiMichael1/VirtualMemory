3876a07b11eeb492984af650a77114b4d6cb2875
andrewman8798 <andrewta8798@gmail.com>
2019-05-02 13:37:26 -0700

    Update PageTable.cpp

--- Hardware/PageTable.cpp
+++ Hardware/PageTable.cpp
@@ -7,12 +7,12 @@ PageTable::PageTable()
 	for( int i = 0 ; i < 256 ; i ++)
 	{
 		pageTable[i].valid = false;
-		pageTables[i].frameNumber = 
+		pageTables[i].frameNumber = 0;
 	}
 }
 
 
 PageTable::~PageTable()
 {
-
+	delete []pagetables;
 }
