9cde220a7ba05abfb3e86d0d69ed8a3143725789
Dave-Patsy <dgwh1995@hotmail.com>
2019-05-08 20:12:50 -0700

    Update Main.cpp

--- Final_MMU/Main.cpp
+++ Final_MMU/Main.cpp
@@ -1,7 +1,23 @@
 #include <iostream>
 #include <fstream>
+#include "PageTable.h"
+#include "Ram.h"
+#include "MemoryManagementUnit.h"
+#include "MemoryManager.h"
+
+static Ram;
+
 
 int main(int argc, char* argv[]) {
 
+	if (argc > 1) {
+		std::cerr <<"to manny arguments\n";
+		return 1;
+	}
+	//1 = fifo
+	//2 = lru
+	if (*argv[1] != '1' && *argv[1] != '2') {
+
+	}
     return 0;
 }
