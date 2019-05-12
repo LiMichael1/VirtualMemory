a829dcb37976adb125fdeff92fe1d7c4f6883131
Michael Li <limichael1099419@gmail.com>
2019-05-12 16:08:48 -0700

    slight changes

--- OS/MemManager.cpp
+++ OS/MemManager.cpp
@@ -1,16 +1,18 @@
 void MemManager::pagein(Page& p, BackingStore bs)
 {
 	//frameNumber  //= PRA
-	bs.read()//overwrite RAM data
+	//overwrite RAM data
+	
+    //when there's a page fault
+    FIFO f ;
+    Word frame = f.findVictim(pt, p);
+
+    bs.read(p, Memory[frame.uint32()]);
+    
+
 	//searchPT[Framenumber].valid = false;
 	//PT[Frame].valid = True;
 
-    //when there's a page fault
-    FIFO f ;
-    Frame frame = f.findVictim(/* pagetable*/ );
-
-    bs.read(p, Memory[frame]);
-    //search in the Page table
     
 
 } 
---
+++ main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+
+int main( )
+{
+	MemoryManagementUnit mmu;
+	PageTable pt;
+
+
+
+	while()//reading address.txt
+	{
+		//Returns
+		try
+		{
+			mmu.read();
+		}
+		catch (PageFault& pf)
+		{
+			//mm.pagein(pf.pagenumber)
+		}
+
+	}
+
+
+	return 0;
+}
+
+
+
+
+
+
+
