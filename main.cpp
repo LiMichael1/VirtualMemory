1b5f345558b63eb81986971e6f49ae5988ab9323
Michael Li <limichael1099419@gmail.com>
2019-05-12 16:18:08 -0700

    close

--- main.cpp
+++ main.cpp
@@ -1,31 +1,57 @@
 #include <iostream>
-#include <vector>
+#include <fstream>
+#include "PageTable.h"
+#include "Ram.h"
+#include "MemoryManagementUnit.h"
+#include "MemoryManager.h"
+#include "Address.h"
 
-int main( )
-{
-	MemoryManagementUnit mmu;
-	PageTable pt;
+static Ram ram;
+static BackingStore bs;
 
+int main(int argc, char* argv[]) {
 
+    Address logical;
+    MemoryManagementUnit MMU;
+    MemoryManager MM;
+    PageTable pt;
+    char data;
 
-	while()//reading address.txt
-	{
-		//Returns
-		try
-		{
-			mmu.read();
-		}
-		catch (PageFault& pf)
-		{
-			//mm.pagein(pf.pagenumber)
-		}
+    if (argc > 1) {
+        std::cerr <<"to manny arguments\n";
+        return 1;
+    }
+    //1 = fifo
+    //2 = lru
+    if (*argv[1] != '1' && *argv[1] != '2') {
 
-	}
+    }
 
+    //open file stream to addresses.txt
+    std::ifstream addr;
+    addr.open("C:\\Users\\handb\\Desktop\\CPSC 251 Final\\VirtualMemory\\addresses.txt");
+    if (!addr) {
+        std::cout << "was not able to open addresses.txt\n";
+        exit(1);
+    }
 
-	return 0;
+    while (addr >> logical)
+    {
+        try {
+            MMU.read(pt, logical);
+            ram.read(logical, data);
+        }
+        catch (PageFault x) {
+            MM.pageIn(pt, logical);
+
+            ram.read(logical, data);
+        }
+
+        std::cout << data << std::endl;
+        
+    }
+    return 0;
 }
-
 
 
 
